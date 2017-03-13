bfd4deba36e415ac98c6bcb6129201b1b8759eb9
gerard <gerar.f87@gmail.com>
2017-03-13 17:43:55 +0100

    Add new message to FAB

--- app/src/main/java/com/android/udl/locationoffers/MainActivity.java
+++ app/src/main/java/com/android/udl/locationoffers/MainActivity.java
@@ -1,15 +1,10 @@
 package com.android.udl.locationoffers;
 
-import android.*;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -18,7 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {

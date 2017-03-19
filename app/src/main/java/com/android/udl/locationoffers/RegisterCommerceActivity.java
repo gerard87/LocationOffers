5199262220dc1ad43fd010f729b445b0a7ee75c9
gerard <gerar.f87@gmail.com>
2017-03-19 16:30:16 +0100

    Back button to registration

--- app/src/main/java/com/android/udl/locationoffers/RegisterCommerceActivity.java
+++ app/src/main/java/com/android/udl/locationoffers/RegisterCommerceActivity.java
@@ -15,11 +15,14 @@ import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.os.AsyncTaskCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -58,6 +61,11 @@ public class RegisterCommerceActivity extends AppCompatActivity implements Googl
         setContentView(R.layout.activity_register_commerce);
 
         buildGoogleApiClient();
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         csh = new CommercesSQLiteHelper(getApplicationContext(), "DBCommerces", null, 1);
 
@@ -101,6 +109,18 @@ public class RegisterCommerceActivity extends AppCompatActivity implements Googl
                 displayPlacePicker();
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void saveToDatabase () {

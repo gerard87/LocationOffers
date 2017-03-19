f28f4fc8d540f1edbd0b4096503374fdd71e83ca
Jordi Mèlich <jordicanon@gmail.com>
2017-03-19 13:36:07 +0100

    implemented PlacePicker on register

--- app/src/main/java/com/android/udl/locationoffers/RegisterCommerceActivity.java
+++ app/src/main/java/com/android/udl/locationoffers/RegisterCommerceActivity.java
@@ -13,10 +13,13 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.os.AsyncTaskCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,8 +28,17 @@ import android.widget.Toast;
 
 import com.android.udl.locationoffers.Utils.BitmapUtils;
 import com.android.udl.locationoffers.database.CommercesSQLiteHelper;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
-public class RegisterCommerceActivity extends AppCompatActivity {
+public class RegisterCommerceActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks{
+
+    private GoogleApiClient mGoogleApiClient;
+    private static final int PLACE_PICKER_REQUEST = 2;
 
     private static final int PERMISSION_EXTERNAL_STORAGE = 1;
     private static final int PICK_IMAGE = 1;
@@ -34,7 +46,7 @@ public class RegisterCommerceActivity extends AppCompatActivity {
     private ImageView imageView;
     private EditText et_name, et_pass, et_placesID;
     private Bitmap bitmap;
-    private Button btn_img, btn_ok;
+    private Button btn_img, btn_ok, btn_placesID;
     private CommercesSQLiteHelper csh;
     private SharedPreferences sharedPreferences;
     private long id;
@@ -44,6 +56,8 @@ public class RegisterCommerceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_commerce);
 
+        buildGoogleApiClient();
+
         csh = new CommercesSQLiteHelper(getApplicationContext(), "DBCommerces", null, 1);
 
         et_name = (EditText) findViewById(R.id.editText_register_name);
@@ -51,6 +65,7 @@ public class RegisterCommerceActivity extends AppCompatActivity {
         et_pass = (EditText) findViewById(R.id.editText_register_password);
         btn_img = (Button) findViewById(R.id.button_form_image);
         btn_ok = (Button) findViewById(R.id.button_register_ok);
+        btn_placesID = (Button) findViewById(R.id.button_selectPlacesID);
         imageView = (ImageView) findViewById(R.id.image_form);
 
         sharedPreferences = getSharedPreferences("my_preferences", Context.MODE_PRIVATE);
@@ -77,6 +92,13 @@ public class RegisterCommerceActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 saveToDatabase();
+            }
+        });
+
+        btn_placesID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayPlacePicker();
             }
         });
     }
@@ -116,6 +138,10 @@ public class RegisterCommerceActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
         super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
 
+        if( requestCode == PLACE_PICKER_REQUEST && resultCode == RESULT_OK ) {
+            PlacePicker.getPlace(imageReturnedIntent, this);
+        }
+
         switch(requestCode) {
             case PICK_IMAGE:
                 if(resultCode == Activity.RESULT_OK){
@@ -134,7 +160,11 @@ public class RegisterCommerceActivity extends AppCompatActivity {
 
                     imageView.setImageBitmap(bitmap);
 
-                }
+                }break;
+            case PLACE_PICKER_REQUEST:
+                String placeID = PlacePicker.getPlace(imageReturnedIntent, this).getId();
+                et_placesID.setText(placeID);
+                break;
         }
     }
 
@@ -147,5 +177,66 @@ public class RegisterCommerceActivity extends AppCompatActivity {
 
         startActivity(new Intent(getApplicationContext(), MainActivity.class));
         finish();
+    }
+
+
+    //RELATED TO GOOGLE API
+
+
+    private void displayPlacePicker() {
+        if( mGoogleApiClient == null || !mGoogleApiClient.isConnected() )
+            return;
+
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+        try {
+            startActivityForResult( builder.build( this ), PLACE_PICKER_REQUEST );
+        } catch ( GooglePlayServicesRepairableException e ) {
+            Log.d( "PlacesAPI Demo", "GooglePlayServicesRepairableException thrown" );
+        } catch ( GooglePlayServicesNotAvailableException e ) {
+            Log.d( "PlacesAPI Demo", "GooglePlayServicesNotAvailableException thrown" );
+        }
+    }
+
+    private void buildGoogleApiClient(){
+        //--Snippet
+        mGoogleApiClient = new GoogleApiClient
+                .Builder( this )
+                .enableAutoManage( this, 0, this )
+                .addApi( Places.GEO_DATA_API )
+                .addApi( Places.PLACE_DETECTION_API )
+                .addConnectionCallbacks( this )
+                .addOnConnectionFailedListener( this )
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if( mGoogleApiClient != null )
+            mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        if( mGoogleApiClient != null && mGoogleApiClient.isConnected() ) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i("ON CONNECTION FAILED","Google Places API connection failed with error code:");
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
     }
 }

8606dbb1a9d530752c77d69de31eeb3177e3a434
gerard87 <gerar.f87@gmail.com>
2017-04-23 14:36:13 +0200

    Places interest fragment: clean and remove warnings

--- app/src/main/java/com/android/udl/locationoffers/fragments/PlacesInterestsFragment.java
+++ app/src/main/java/com/android/udl/locationoffers/fragments/PlacesInterestsFragment.java
@@ -3,8 +3,8 @@ package com.android.udl.locationoffers.fragments;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,8 +13,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.udl.locationoffers.R;
 import com.android.udl.locationoffers.domain.PlaceInterest;
@@ -25,60 +23,19 @@ import java.util.ArrayList;
 
 import static android.content.Context.MODE_PRIVATE;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PlacesInterestsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PlacesInterestsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class PlacesInterestsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     Context myContext;
     PlaceInterestAdapter dataAdapter = null;
     ListView lv;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
     public PlacesInterestsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PlacesInterestsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PlacesInterestsFragment newInstance(String param1, String param2) {
-        PlacesInterestsFragment fragment = new PlacesInterestsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
         myContext = getContext();
     }
 
@@ -91,32 +48,12 @@ public class PlacesInterestsFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstance){
-        lv = (ListView) getView().findViewById(R.id.listView1);
-        displayListView();
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+        if (getView() != null ) {
+            lv = (ListView) getView().findViewById(R.id.listView1);
         }
-    }
-
-    /*@Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+        if (lv != null) {
+            displayListView();
         }
-    }*/
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
     }
 
 
@@ -134,30 +71,13 @@ public class PlacesInterestsFragment extends Fragment {
         super.onPause();
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
-
-
-
 
     private class PlaceInterestAdapter extends ArrayAdapter<PlaceInterest> {
 
         private ArrayList<PlaceInterest> interestList;
 
-        public PlaceInterestAdapter(Context context, int textViewResourceId,
-                               ArrayList<PlaceInterest> interestList) {
+        PlaceInterestAdapter(Context context, int textViewResourceId,
+                             ArrayList<PlaceInterest> interestList) {
             super(context, textViewResourceId, interestList);
             this.interestList = new ArrayList<>();
             this.interestList.addAll(interestList);
@@ -167,8 +87,9 @@ public class PlacesInterestsFragment extends Fragment {
             CheckBox name;
         }
 
+        @NonNull
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
 
             ViewHolder holder;
             Log.i("ConvertView", String.valueOf(position));
@@ -176,7 +97,7 @@ public class PlacesInterestsFragment extends Fragment {
             if (convertView == null) {
                 LayoutInflater vi = (LayoutInflater) myContext.getSystemService(
                         Context.LAYOUT_INFLATER_SERVICE);
-                convertView = vi.inflate(R.layout.interest_place_layout, null);
+                convertView = vi.inflate(R.layout.interest_place_layout, parent, false);
 
                 holder = new ViewHolder();
                 holder.name = (CheckBox) convertView.findViewById(R.id.checkBox1);
@@ -191,7 +112,7 @@ public class PlacesInterestsFragment extends Fragment {
                         SharedPreferences pref = myContext.getSharedPreferences(getString(R.string.PREFERENCES_NAME), MODE_PRIVATE);
                         SharedPreferences.Editor editor = pref.edit();
                         editor.putBoolean(((PlaceInterest)cb.getTag()).getName(),cb.isChecked());
-                        editor.commit();
+                        editor.apply();
                     }
                 });
             }
@@ -224,36 +145,4 @@ public class PlacesInterestsFragment extends Fragment {
         dataAdapter = new PlaceInterestAdapter(myContext,R.layout.interest_place_layout,interestList);
         lv.setAdapter(dataAdapter);
     }
-
-
-
-    /*private void checkButtonClick() {
-
-
-        Button myButton = (Button) findViewById(R.id.findSelected);
-        myButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                StringBuffer responseText = new StringBuffer();
-                responseText.append("The following were selected...\n");
-
-                ArrayList<PlaceInterest> countryList = dataAdapter.countryList;
-                for(int i=0;i<countryList.size();i++){
-                    PlaceInterest country = countryList.get(i);
-                    if(country.isSelected()){
-                        responseText.append("\n" + country.getName());
-                    }
-                }
-
-                Toast.makeText(getApplicationContext(),
-                        responseText, Toast.LENGTH_LONG).show();
-
-            }
-        });
-
-    }*/
-
-//}
 }
